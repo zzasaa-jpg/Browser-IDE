508ca741570ee3b87fcbf441bc9171eb7a92d0bc
Mahendra Kumar <zzsdrt354@gmail.com>
2026-06-11 13:51:26 +0530

    feat: add recursive folder tree rendering.
    ~ Added recursive folder/file rendering in sidebar.
    ~ Separated file/folder container from children container.
    ~ Added required tree-view CSS styling.
    
    Signed-off-by: Mahendra Kumar <zzsdrt354@gmail.com>

--- src/components/FileItem/FileItem.jsx
+++ src/components/FileItem/FileItem.jsx
@@ -1,16 +1,32 @@
 import "./FileItem.css";
+import { useState } from "react";
 
 export default function FileItem({ file }) {
-	return (
-		<div className="file_or_folder_name">
-			{file.type == "directory" ? <img className="Directory_img" src="src/assets/folder-outline.svg" alt="icon" width="20" height="20" /> : <img className="File_img" src="src/assets/document-text-outline.svg" alt="icon" width="20" height="20" />}
-			{file.name}
+	const [open, setOpen] = useState(false);
+	const isFolder = file.type === "directory";
 
-			{/* {
-		  file.children?.map(child => (
-			  <FileItem key={child.name} file={child}/>
-		  ))
-	  } */}
-		</div>
+	return (
+		<>
+			<div className="file_or_folder_name" onClick={() => isFolder && setOpen(!open)}>
+				{
+					file.type == "directory" ?
+						<img className="Directory_img" src="src/assets/folder-outline.svg" alt="icon" width="20" height="20" /> :
+						<img className="File_img" src="src/assets/document-text-outline.svg" alt="icon" width="20" height="20" />
+				}
+				<span className="file_name_text">
+					{file.name}
+				</span>
+			</div>
+			<div className="children_container">
+				{
+					open &&
+					file.children?.map(child => (
+						<div key={child.name}>
+							<FileItem file={child} />
+						</div>
+					))
+				}
+			</div>
+		</>
 	);
 };
